d86b6eb68e0f7a1bf8f41ee2448c4adfb257fe8c
Alexander Smirnov <54988303+0Meridian0@users.noreply.github.com>
2021-08-07 09:55:05 +0300

    An attempt to fix the block motion controller

--- MoBlox/Assets/Scripts/Player.cs
+++ MoBlox/Assets/Scripts/Player.cs
@@ -27,17 +27,30 @@ public class Player : MonoBehaviour
             if (Physics.Raycast(ray, out RaycastHit hit, 100))
             {
                 Vector3 point;
+                float valCord;
                 if (isHorizontal)
                 {
-                    point = new Vector3(transform.position.x,
-                        transform.position.y,
-                        Mathf.Round(hit.point.z));
+                    valCord = Mathf.Abs(transform.position.z / 0.5);
+                    if (valCord / 2 == 0)
+                    {
+                        point = new Vector3(transform.position.x, transform.position.y, Mathf.Round(hit.point.z));
+                    }
+                    else
+                    {
+                        point = new Vector3(transform.position.x, transform.position.y, Mathf.Round(hit.point.z)+0.5);
+                    }
                 }
                 else
                 {
-                    point = new Vector3(Mathf.Round(hit.point.x),
-                        transform.position.y,
-                        transform.position.z);
+                    valCord = Mathf.Abs(transform.position.x / 0.5);
+                    if (valCord / 2 == 0)
+                    {
+                        point = new Vector3(Mathf.Round(hit.point.x), transform.position.y, transform.position.z);
+                    }
+                    else
+                    {
+                        point = new Vector3(Mathf.Round(hit.point.x) + 0.5, transform.position.y, transform.position.z);
+                    }
                 }
 
                 if (Physics.Raycast(transform.position, (point - transform.position).normalized, out RaycastHit hit2,
